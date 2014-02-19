df9700ed1c9bbc203c0b1a66d4829318d025bd06
likerRr <a.liker@mail.ru>
2014-02-19 18:53:23 +0300

    [add] test example [add] libraries

---
+++ cutter.js
@@ -0,0 +1,169 @@
+(function ($) {
+    "use strict";
+    // last element that will mouseover/mouseout
+    var $lastElement,
+    // last parent container for $lastElement
+        $lastInstance;
+
+    // add handle to keys to cut elements
+    $(document).on('keypress', function(e){
+        var key = e.keyCode,
+            symbolicKey = e.which,
+        // "delete" key
+            deleteKeyCode = 46,
+        // "control" key
+            ctrlKey = e.ctrlKey,
+        // "x" key
+            xKeyCode = 120;
+        if ($lastElement !== undefined && $lastElement.length == 1) {
+            if (key === deleteKeyCode || (symbolicKey == xKeyCode && ctrlKey == true)) {
+                // push deleted item in array to subsequent access
+                $lastInstance.deleted.push($lastElement);
+//					$lastElement.fadeOut('fast');
+                methods._onCut($lastElement);
+                methods._afterCut($lastElement);
+            }
+        }
+    });
+
+    var methods = {
+        init: function(options) {
+            var opts = $.extend({
+                // function that will handle after cutting
+                afterCut: undefined,
+                // function that will handle when mouse moves over the element
+                mouseOver: undefined,
+                // function that will handle when mouse moves out ot element
+                mouseOut: undefined,
+                onCut: undefined,
+                onRestore: undefined
+            }, options);
+
+            var $instance = {
+                // elements that can be cut
+                container: this,
+                options: opts,
+                // deleted child inside container
+                deleted: []
+            };
+            this.cutter('start');
+            this.data('instance', $instance);
+
+            return this;
+        },
+        start: function() {
+            var $instance = this.data('instance');
+            if ($instance) {
+                $instance.active = 1;
+                $instance.container.on('mouseover', function(e){
+                    if ($instance.active == 1) {
+                        var $target = $(e.target);
+                        $lastElement = $target;
+                        $lastInstance = $instance;
+                        methods._mouseOver($target);
+                    }
+                }).on('mouseout', function(e){
+                    if ($instance.active == 1) {
+                        var $target = $(e.target);
+                        $lastElement = undefined;
+                        $lastInstance = $instance;
+                        methods._mouseOut($target);
+                    }
+                });
+            }
+            this.data('instance', $instance);
+
+            return this;
+        },
+        stop: function() {
+            var $instance = this.data('instance');
+            $instance.active = 0;
+            this.data('instance', $instance);
+
+            return this;
+        },
+        deleted: function() {
+            var $instance = this.data('instance');
+            return $instance.deleted;
+        },
+        // restore one or all elements
+        restore: function($el) {
+            var $instance = this.data('instance'),
+                deletedItems = $instance.deleted;
+
+            for (var i = 0; i < deletedItems.length; i++) {
+                // one element
+                if ($el != undefined) {
+                    // if equals element in deleted array
+                    if ($el == deletedItems[i]) {
+                        methods._onRestore(deletedItems[i]);
+                        break;
+                    }
+                }
+                else {
+                    methods._onRestore(deletedItems[i]);
+                }
+            }
+
+            return this;
+        },
+        _afterCut: function(justDeleted) {
+            if ($lastInstance.options.afterCut !== undefined) {
+                $lastInstance.options.afterCut(justDeleted);
+            }
+        },
+        _mouseOver: function($target) {
+            if ($lastInstance.options.mouseOver != undefined) {
+                $lastInstance.options.mouseOver($target);
+            }
+            else {
+                $target
+                    .data('border', $target.css('border'))
+                    .css({
+                        border: 'solid #e74c3c 1px',
+                        cursor: 'crosshair'
+                    });
+            }
+        },
+        _mouseOut: function($target) {
+            if ($lastInstance.options.mouseOut != undefined) {
+                $lastInstance.options.mouseOut($target);
+            }
+            else {
+                $target.css({
+                    border: $target.data('border')
+                });
+            }
+        },
+        _onCut: function($target) {
+            if ($lastInstance.options.onCut != undefined) {
+                $lastInstance.options.onCut($target);
+            }
+            else {
+                $target.fadeOut('fast');
+            }
+        },
+        _onRestore: function($target) {
+            if ($lastInstance.options.onRestore != undefined) {
+                $lastInstance.options.onRestore($target);
+            }
+            else {
+                $target.fadeIn('fast');
+            }
+        }
+    };
+
+    $.fn.cutter = function(method) {
+        if (methods[method]) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        }
+        else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        }
+        else {
+            $.error('Method ' +  method + ' not used in $.cutter' );
+
+            return false;
+        }
+    };
+})(jQuery);
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+	$content1 = file_get_contents('http://yahoo.com');
+	$content2 = file_get_contents('http://mail.ru');
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cutterjs</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="cutter.min.js"></script>
+</head>
+<body>
+<div id="example1">
+	<?php echo $content1; ?>
+</div>
+<div id="example2">
+	<?php echo $content2; ?>
+</div>
+<script>
+	var $lastDeleted;
+	// custom initialization
+	var $a = $('#example1')
+		.cutter('init', {
+			afterCut: function(a) {$lastDeleted = a;},
+			mouseOver: function(a) {
+				a.data('border', a.css('border'));
+				a.css('border', 'dotted gray 1px');
+			},
+			mouseOut: function(a) {
+				a.css('border', a.data('border'));
+			},
+			onCut: function(a) {
+				a.data('background', a.css('background'));
+				a.css('background', 'black');
+			},
+			onRestore: function(a) {
+				a.css('background', a.data('background'));
+			}
+		})
+		.cutter('stop')
+		.cutter('start');
+
+	// default initialization
+	var $b = $('#example2').cutter('init').cutter('stop').cutter('start');
+
+	setTimeout(function(){
+		// debug deleted items
+		console.log($a.cutter('deleted'));
+		console.log($b.cutter('deleted'));
+	}, 5000);
+
+	setTimeout(function(){
+		// restore last
+		$a.cutter('restore', $lastDeleted);
+		// restore all
+		$b.cutter('restore');
+	}, 6000);
+</script>
+</body>
+</html>
